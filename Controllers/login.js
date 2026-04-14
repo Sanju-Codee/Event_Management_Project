0bedfaab59b41152d291e22260e7a5e912b3aa8f
Sanju-Codee <142675968+Sanju-Codee@users.noreply.github.com>
2026-04-14 19:48:45 +0530

    Update login.js

--- Controllers/login.js
+++ Controllers/login.js
@@ -4,11 +4,11 @@ import { StatusCodes } from "http-status-pro-js"
 import jwt from "jsonwebtoken"
 async function login(req, res) {
     try {
-        const {_id,password } = req.body
-        if(!_id ||!password){
+        const {email,password } = req.body
+        if(!email ||!password){
             return res.status(400).send("All field are required")
         }
-        const user = await User.findById(_id)
+        const user = await User.findById(email)
         if (!user) {
             return res.status(404).send("User not found")
         }
